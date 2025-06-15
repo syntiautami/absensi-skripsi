84a8578a2b09fcd5a677961ccc487d54d174d8bb
mas-paw <fauzi6409@gmail.com>
2025-06-15 22:25:28 +0700

    fix: admin lte 4

--- app/Views/layout/base.php
+++ app/Views/layout/base.php
@@ -5,35 +5,44 @@
   <meta charset="UTF-8">
   <title><?= $this->renderSection('title') ?></title>
 
-  <!-- CSS AdminLTE -->
-  <link rel="stylesheet" href="<?= base_url('adminlte/plugins/fontawesome-free/css/all.min.css') ?>">
-  <link rel="stylesheet" href="<?= base_url('adminlte/css/adminlte.min.css') ?>">
+  <!-- Font Awesome (optional) -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  
+  <!-- AdminLTE CSS -->
+  <link rel="stylesheet" href="<?= base_url('assets/adminlte/css/adminlte.min.css') ?>">
 
   <?= $this->renderSection('styles') ?>
 </head>
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+  <div class="wrapper">
+    <!-- Navbar -->
+    <?= $this->renderSection('navbar') ?>
 
-  <!-- Navbar -->
-  <?= $this->renderSection('navbar') ?>
+    <!-- Sidebar -->
+    <?= $this->renderSection('sidebar') ?>
 
-  <!-- Sidebar -->
-  <?= $this->renderSection('sidebar') ?>
+    <!-- Content -->
+    <div class="content-wrapper p-3">
+      <?= $this->renderSection('content') ?>
+    </div>
 
-  <!-- Content -->
-  <div class="content-wrapper p-3">
-    <?= $this->renderSection('content') ?>
+    <!-- Footer -->
+    <?= $this->renderSection('footer') ?>
   </div>
 
-  <!-- Footer -->
-  <?= $this->renderSection('footer') ?>
-</div>
+  <!-- jQuery -->
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
-<!-- JS AdminLTE -->
-<script src="<?= base_url('adminlte/plugins/jquery/jquery.min.js') ?>"></script>
-<script src="<?= base_url('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
-<script src="<?= base_url('adminlte/js/adminlte.min.js') ?>"></script>
+  <!-- Bootstrap JS Bundle (sudah termasuk Popper) -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
-<?= $this->renderSection('scripts') ?>
+  <!-- AdminLTE App -->
+  <script src="<?= base_url('assets/adminlte/js/adminlte.min.js') ?>"></script>
+
+  <!-- Custom Scripts -->
+  <?= $this->renderSection('scripts') ?>
 </body>
 </html>
