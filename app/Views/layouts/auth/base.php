45bc4e75f3d8d50f94ecede0932de27d00673058
mas-paw <fauzi6409@gmail.com>
2025-06-19 17:51:32 +0700

    login

--- app/Views/layouts/auth/base.php
+++ app/Views/layouts/auth/base.php
@@ -18,6 +18,29 @@
   <script src="<?= base_url('assets/adminlte/plugins/jquery/jquery.min.js') ?>"></script>
   <script src="<?= base_url('assets/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
   <script src="<?= base_url('assets/adminlte/js/adminlte.min.js') ?>"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <?php if (session()->getFlashdata('success')): ?>
+    <script>
+        Swal.fire({
+            icon: 'success',
+            title: 'Sukses',
+            text: '<?= session()->getFlashdata('success') ?>',
+            timer: 3000,
+            showConfirmButton: false
+        });
+    </script>
+  <?php endif; ?>
+
+  <?php if (session()->getFlashdata('error')): ?>
+    <script>
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: '<?= session()->getFlashdata('error') ?>',
+            timer: 3000,
+        });
+    </script>
+  <?php endif; ?>
   <?= $this->renderSection('scripts') ?>
 </body>
 </html>
