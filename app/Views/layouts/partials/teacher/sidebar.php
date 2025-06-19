c388b88ada3dbd43cde134b83d2b9913af0cea3a
mas-paw <fauzi6409@gmail.com>
2025-06-19 17:45:40 +0700

    awdawd

--- app/Views/layouts/partials/teacher/sidebar.php
+++ app/Views/layouts/partials/teacher/sidebar.php
@@ -5,7 +5,7 @@
          alt="Logo" 
          class="brand-image img-circle elevation-3"
          style="opacity: .8">
-    <span class="brand-text font-weight-light">Sistem Absensi</span>
+    <span class="brand-text">Sistem Absensi</span>
   </a>
 
   <div class="sidebar">
