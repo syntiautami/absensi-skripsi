d4757649aa94d0cfb1baedfd788c361c4011d613
mas-paw <fauzi6409@gmail.com>
2025-06-24 00:16:37 +0700

    fix: img

--- app/Controllers/Admin/Main.php
+++ app/Controllers/Admin/Main.php
@@ -9,82 +9,82 @@ class Main extends BaseController
 {
     public function index()
     {
-        $walas = session()->get('homeroom_teacher');
+        // $walas = session()->get('homeroom_teacher');
 
-        $studentStatisticData = [
-            'present' =>0,
-            'late' => 0,
-            'sick' => 0,
-            'excused' => 0,
-            'absent' => 0,
-        ];
-        if ($walas) {
-            $csId = $walas['class_semester_id'];
-            $scsModel = new StudentClassSemesterModel();
-            $scsData = $scsModel->getByClassSemesterId($csId);
-            $profileIds = array_column($scsData, 'profile_id');
-            $scsIds = array_column($scsData, 'id');
+        // $studentStatisticData = [
+        //     'present' =>0,
+        //     'late' => 0,
+        //     'sick' => 0,
+        //     'excused' => 0,
+        //     'absent' => 0,
+        // ];
+        // if ($walas) {
+        //     $csId = $walas['class_semester_id'];
+        //     $scsModel = new StudentClassSemesterModel();
+        //     $scsData = $scsModel->getByClassSemesterId($csId);
+        //     $profileIds = array_column($scsData, 'profile_id');
+        //     $scsIds = array_column($scsData, 'id');
 
-            $dailyEntryModel = new AttendanceDailyEntryModel();
-            $todayEntries = $dailyEntryModel->getTodayEntriesByProfileIds($profileIds);
-            $attModel = new AttendanceModel();
-            $todayAttendance = $attModel -> getTodayAttendanceByscsId($scsIds);
+        //     $dailyEntryModel = new AttendanceDailyEntryModel();
+        //     $todayEntries = $dailyEntryModel->getTodayEntriesByProfileIds($profileIds);
+        //     $attModel = new AttendanceModel();
+        //     $todayAttendance = $attModel -> getTodayAttendanceByscsId($scsIds);
 
-            $studentAttendanceToday = [];
-            $AttendancestudentLate = [];
-            foreach ($todayAttendance as $item) {
-                $status = $item['attendance_type_id'];
-                $profileId = $item['profile_id'];
+        //     $studentAttendanceToday = [];
+        //     $AttendancestudentLate = [];
+        //     foreach ($todayAttendance as $item) {
+        //         $status = $item['attendance_type_id'];
+        //         $profileId = $item['profile_id'];
 
-                if (!isset($studentAttendanceToday[$status])) {
-                    $studentAttendanceToday[$status] = [];
-                }
+        //         if (!isset($studentAttendanceToday[$status])) {
+        //             $studentAttendanceToday[$status] = [];
+        //         }
 
-                $studentAttendanceToday[$status][] = $profileId;
+        //         $studentAttendanceToday[$status][] = $profileId;
 
-                if ($status == 4) {
-                    $AttendancestudentLate[] = $profileId;
-                }
+        //         if ($status == 4) {
+        //             $AttendancestudentLate[] = $profileId;
+        //         }
 
-                if (isset($studentStatisticData[AttendanceHelper::ATTENDANCE_TYPE_MAPPING[$status]])) {
-                    $studentStatisticData[AttendanceHelper::ATTENDANCE_TYPE_MAPPING[$status]]++;
-                }
-                // $studentStatisticData['total']++;
+        //         if (isset($studentStatisticData[AttendanceHelper::ATTENDANCE_TYPE_MAPPING[$status]])) {
+        //             $studentStatisticData[AttendanceHelper::ATTENDANCE_TYPE_MAPPING[$status]]++;
+        //         }
+        //         // $studentStatisticData['total']++;
 
-            }
-            foreach ($todayEntries as $entry) {
-                if (!isset($studentAttendance[$entry['profile_id']])) {
-                    $entry['status'] = 'present';
-                    $studentStatisticData['present']++;
-                    // $studentStatisticData['total']++;
-                }
-            }
-        }
+        //     }
+        //     foreach ($todayEntries as $entry) {
+        //         if (!isset($studentAttendance[$entry['profile_id']])) {
+        //             $entry['status'] = 'present';
+        //             $studentStatisticData['present']++;
+        //             // $studentStatisticData['total']++;
+        //         }
+        //     }
+        // }
 
-        $profileId = session()->get('user')['profile_id'];
-        $teacherModel = new TeacherModel();
-        $teacher = $teacherModel->getDataByProfileId($profileId);
+        // $profileId = session()->get('user')['profile_id'];
+        // $teacherModel = new TeacherModel();
+        // $teacher = $teacherModel->getDataByProfileId($profileId);
 
-        $tcssModel = new TeacherClassSemesterSubjectModel();
-        $classTimetablePeriodList = [];
-        $teacher_class_semester_subjects = $tcssModel-> getInSessionTcssByTeacher($teacher['id']);
-        if (!empty($teacher_class_semester_subjects)) {
-            $cssIds = array_column($teacher_class_semester_subjects, 'css_id');
-            $ctpModel = new ClassTimetablePeriodModel();
-            $ctpList = $ctpModel-> getActiveByCssIds($cssIds);
+        // $tcssModel = new TeacherClassSemesterSubjectModel();
+        // $classTimetablePeriodList = [];
+        // $teacher_class_semester_subjects = $tcssModel-> getInSessionTcssByTeacher($teacher['id']);
+        // if (!empty($teacher_class_semester_subjects)) {
+        //     $cssIds = array_column($teacher_class_semester_subjects, 'css_id');
+        //     $ctpModel = new ClassTimetablePeriodModel();
+        //     $ctpList = $ctpModel-> getActiveByCssIds($cssIds);
 
-            foreach ($ctpList as $data) {
-                $day = $data['day'];
+        //     foreach ($ctpList as $data) {
+        //         $day = $data['day'];
                 
-                if (!isset($classTimetablePeriodList[$day])) {
-                    $classTimetablePeriodList[$day] = [];
-                }
-                $classTimetablePeriodList[$day][] = $data;
-            }
-        }
+        //         if (!isset($classTimetablePeriodList[$day])) {
+        //             $classTimetablePeriodList[$day] = [];
+        //         }
+        //         $classTimetablePeriodList[$day][] = $data;
+        //     }
+        // }
         
         return view('admin/home', [
-            'attendance_data' => $studentStatisticData,
+            // 'attendance_data' => $studentStatisticData,
             'viewing' => 'dashboard',
         ]);
     }
