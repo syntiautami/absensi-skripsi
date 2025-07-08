0630f4e458053dfea114349a6603f572b8de9c7f
mas-paw <fauzi6409@gmail.com>
2025-07-08 22:18:58 +0700

    fix

--- app/Views/admin/classes/class_semester/edit.php
+++ app/Views/admin/classes/class_semester/edit.php
@@ -55,7 +55,10 @@
                     </div>
                 </div>
                 <div class="card-footer">
-                    <button type="submit" class="btn btn-primary">Simpan</button>
+                    <div class="d-flex justify-content-end mt-3">
+                        <a href="<?= base_url('admin/classes/academic-year/'.$academic_year['id'].'/class-semester-year/'.$class_semester_year['id'].'/') ?>" class="btn btn-secondary mr-2">Kembali</a>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
                 </div>
             </form>
         </div>
