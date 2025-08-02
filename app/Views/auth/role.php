0c9134edb9df33e226daccffeae88534638389ae
mas-paw <fauzi6409@gmail.com>
2025-08-02 19:55:02 +0700

    fix role

--- app/Views/auth/role.php
+++ app/Views/auth/role.php
@@ -1,12 +1,19 @@
 <?= $this->extend('layouts/auth/base') ?>
 <?= $this->section('content') ?>
 <div class="container text-center mt-5">
-  <h2>Pilih Role</h2>
-  <div class="row justify-content-center mt-4">
+  <div class="row justify-content-center">
+    <div class="col-md-12 col-xs-12">
+      <img class="text-center" src="<?= base_url('assets/img/logo.png') ?>" 
+          alt="Logo" 
+          class="brand-image img-circle"
+          style="opacity: .8; width:auto; height:150px;">
+    </div>
+  </div>
+  <div class="row justify-content-center mt-5">
     <?php foreach ($roles as $role): ?>
       <div class="col-md-3">
-        <a href="<?= base_url('role/' . $role['name']) ?>/" class="btn btn-primary btn-block mb-3">
-          <?= ucfirst($role['alt_name']) ?>
+        <a href="<?= base_url('role/' . $role['name']) ?>/" class="btn btn-primary btn-lg btn-block mb-3 justify-content-center align-content-center" style="height: 100px;">
+          Pilih Modul <?= ucfirst($role['alt_name']) ?>
         </a>
       </div>
     <?php endforeach ?>
